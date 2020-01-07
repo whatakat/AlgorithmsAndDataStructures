43a548c01bc2d943e3a5e6c0e926b82ce343a872
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-07 23:25:49 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,27 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        //delete element in arrays
-        int[] myArr = {1,2,3,4,5,2};
-        int i;
-        int len = myArr.length;
-        int serch = 4;
-        for (int j = 0; j <len ; j++) {
-            System.out.print(myArr[j]);
-        }
-        for (i = 0; i <len ; i++) {
-            if (myArr[i]==serch) break;
-        }
-        for (int j = i; j <len-1 ; j++) {
-            myArr[j]=myArr[j+1];
-        }
-        len--;
-        System.out.println();
-        for (int j = 0; j <len ; j++) {
-            System.out.print(myArr[j]);
-            
-        }
-        //**********************************************
+
+        MyArr arr = new MyArr(10);
+        arr.insert(5);
+        arr.insert(2);
+        arr.insert(3);
+        arr.insert(4);
+        arr.insert(6);
+        arr.insert(7);
+        arr.insert(8);
+        arr.insert(9);
+        arr.insert(10);
+        arr.insert(1);
+        arr.display();
+        arr.delete(10);
+        arr.display();
+
+
 
     }
 }
