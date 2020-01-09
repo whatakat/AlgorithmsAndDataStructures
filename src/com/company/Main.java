ea07a18698cf710967cf0bda6685b6bae3a30c08
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-09 15:42:19 +0300

    test reference data type

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyArr arr = new MyArr(10);
-        arr.insert(5);
-        arr.insert(2);
-        arr.insert(3);
-        arr.insert(4);
-        arr.insert(6);
-        arr.insert(7);
-        arr.insert(8);
-        arr.insert(9);
-        arr.insert(10);
-        arr.insert(1);
-        int search = 8;
-        System.out.println(arr.binaryFind(search));
+//        MyArr arr = new MyArr(10);
+//        arr.insert(5);
+//        arr.insert(2);
+//        arr.insert(3);
+//        arr.insert(4);
+//        arr.insert(6);
+//        arr.insert(7);
+//        arr.insert(8);
+//        arr.insert(9);
+//        arr.insert(10);
+//        arr.insert(1);
+//        int search = 8;
+//        System.out.println(arr.binaryFind(search));
+        int size = 100;
+        MyArr myArr = new MyArr(size);
+        myArr.insert("Vasya", 10);
+        myArr.insert("Igor",15);
+        myArr.display();
+        myArr.delete("Igor");
+        myArr.display();
 
 
 
