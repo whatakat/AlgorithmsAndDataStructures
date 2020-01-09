838265518df3559a3110f3c8da29e4dffef7fd93
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-09 15:58:40 +0300

    add sort bubble

--- src/com/company/MyArr.java
+++ src/com/company/MyArr.java
@@ -101,6 +101,22 @@ public class MyArr {
         this.arr[this.size] = new Person(name,age);
         this.size++;
     }
-
+    /* SORT BUBBLE
+    public void sortBubble(){
+        int out, in;
+        for (out = this.size-1; out>1 ; out--) {
+            for (in = 0; in <out ;in ++) {
+                if (this.arr[in]>this.arr[in+1]){
+                    change(in,in+1);
+                }
+            }
+        }
+    }
+    public void change(int a, int b){
+        int tmp = this.arr[a];
+        this.arr[a] = this.arr[b];
+        this.arr[b] = tmp;
+    }
+*/
 
 }
