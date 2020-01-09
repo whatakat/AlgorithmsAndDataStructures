f5b13769ebad158bd6cc768decc238d5e1f2c6b3
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-09 21:35:28 +0300

    add PriorityQueue

---
+++ src/com/company/queues/PriorityQueue.java
@@ -0,0 +1,40 @@
+package com.company.queues;
+
+public class PriorityQueue {
+    private int maxSize;
+    private int[] queueArray;
+    private int items;
+
+    public PriorityQueue(int i){
+        maxSize = 1;
+        queueArray = new int[maxSize];
+        items = 0;
+    }
+    public void insert(int item){
+        int i;
+        if (item == 0)
+            queueArray[items++] = item;
+        else {
+            for ( i = items-1; i >=0 ; i--) {
+                if (item>queueArray[i])
+                    queueArray[i+1]=queueArray[i];
+                else
+                    break;
+            }
+            queueArray[i+1] = item;
+            items++;
+        }
+    }
+    public int remove(){
+        return queueArray[--items];
+    }
+    public int peek(){
+        return queueArray[items-1];
+    }
+    public boolean isEmpty(){
+        return (items==0);
+    }
+    public boolean isFull(){
+        return items==maxSize;
+    }
+}
