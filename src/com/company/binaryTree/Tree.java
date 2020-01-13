4aebcfbca4727f536543a28ab30e482234315498
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-13 15:44:05 +0300

    add class Tree

---
+++ src/com/company/binaryTree/Tree.java
@@ -0,0 +1,18 @@
+package com.company.binaryTree;
+
+public class Tree {
+    private Node root;
+
+    public Node find(int key){
+
+    }
+    public void insert(Person person){
+
+    }
+    public boolean delete(int id){
+
+    }
+    public void displayTree(){
+
+    }
+}
