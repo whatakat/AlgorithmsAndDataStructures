dcd36cf63e9473daa1508765b65b929383e6b0b2
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-13 16:31:38 +0300

    method find

--- src/com/company/binaryTree/Tree.java
+++ src/com/company/binaryTree/Tree.java
@@ -4,6 +4,18 @@ public class Tree {
     private Node root;
 
     public Node find(int key){
+        Node current = root;
+        while (current.person.id!=key){
+            if (key<current.person.id){
+                current = current.leftChild;
+            }else {
+                current = current.rightChild;
+            }
+            if (current ==null){
+                return null;
+            }
+        }
+        return current;
 
     }
     public void insert(Person person){
