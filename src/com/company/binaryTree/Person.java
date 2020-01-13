2e1f6b142c3e9dea69e74aeb6e97998cdb9851c8
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-13 15:37:44 +0300

    add class Person

---
+++ src/com/company/binaryTree/Person.java
@@ -0,0 +1,17 @@
+package com.company.binaryTree;
+
+public class Person {
+    public String name;
+    public int id;
+    public int age;
+
+    public Person(){
+
+    }
+    public Person(String name, int id, int age){
+        this.name=name;
+        this.id = id;
+        this.age = age;
+    }
+
+}
