52683d3637597acbdbbe1aca7f5ad8d13913024b
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-14 15:07:34 +0300

    add TreeApp

---
+++ src/com/company/binaryTree/TreeApp.java
@@ -0,0 +1,44 @@
+package com.company.binaryTree;
+
+import java.io.IOException;
+
+public class TreeApp {
+    public static void main(String[] args) throws IOException {
+        int value;
+        Tree theTree = new Tree();
+        theTree.insert(new Person());
+        theTree.insert(new Person());
+        theTree.insert(new Person());
+        theTree.insert(new Person());
+        theTree.insert(new Person());
+        theTree.insert(new Person());
+        theTree.insert(new Person());
+        theTree.insert(new Person());
+        while (true){
+            System.out.println("Enter first  letter of show, ");
+            System.out.println("insert, find, delete, or traverse: ");
+            int choise = getChar();
+            switch (choise){
+                case 's':
+                    theTree.displayTree();
+                    break;
+                case 'i':
+                    System.out.println("Enter value to insert: ");
+                    value = getint();
+                    theTree.insert(new Person());
+                    break;
+                case 'f':
+                    System.out.println("Enter value to find: ");
+                    value = getInt();
+                    Node found = theTree.find(value);
+                    if (found !=null){
+                        System.out.println("Found: ");
+                    }
+                    found.display();
+                    System.out.println("/n");
+                    break;
+            }
+        }
+    }
+
+}
