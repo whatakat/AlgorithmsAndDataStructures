9d7a89d5b0de394c8d99e00ff915869f746f0731
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-14 18:51:48 +0300

    add Graph

---
+++ src/com/company/graph/Graph.java
@@ -0,0 +1,28 @@
+package com.company.graph;
+
+public class Graph {
+    private final int MAX_VERTS = 32;
+    private Vertex[] vertexList;
+    private int[][] adjMat;
+    private int size;
+    private Graph(){
+        vertexList = new Vertex[MAX_VERTS];
+        adjMat = new int[MAX_VERTS][MAX_VERTS];
+        size = 0;
+        for (int i = 0; i <MAX_VERTS ; i++) {
+            for (int j = 0; j <MAX_VERTS ; j++) {
+                adjMat[i][j] = 0;
+            }
+        }
+    }
+    public void addVertex(char label){
+        vertexList[size++]=new Vertex(label);
+    }
+    public void addEdge(int start, int end){
+        adjMat[start][end] = 1;
+        adjMat[end][start]= 1;
+    }
+    public void displayVertex(int vertex){
+        System.out.println(vertexList[vertex]);
+    }
+}
