86a9210d955e9590e89c9a5a172d3a8eaab93f65
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-15 21:52:48 +0300

    add GraphApp

--- src/com/company/graph/GraphApp.java
+++ src/com/company/graph/GraphApp.java
@@ -4,6 +4,17 @@ public class GraphApp {
     public static void main(String[] args) {
         Graph graph = new Graph();
         graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+        graph.addVertex('E');
+
+        graph.addEdge(0,1);//AB
+        graph.addEdge(1,2);//BC
+        graph.addEdge(0,3);//AD
+        graph.addEdge(3,4);//DE
+
+        graph.dfs();
 
 
     }
