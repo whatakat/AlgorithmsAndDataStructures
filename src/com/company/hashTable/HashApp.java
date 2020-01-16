8b2cdc336de3a80d524584f1cca332241ef2c168
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-16 23:49:22 +0300

    HashApp

--- src/com/company/hashTable/HashApp.java
+++ src/com/company/hashTable/HashApp.java
@@ -12,10 +12,44 @@ public class HashApp {
         size = getInt();
         System.out.println("Enter initial number of items: ");
         n = getInt();
-
-
-
-
+        keysPerCell = 10;
+        HashTable theHashTable = new HashTable(size);
+        for (int i = 0; i <n ; i++) {
+            aKey = (int)(java.lang.Math.random()*keysPerCell*size);
+            aDataItem = new Item(aKey);
+        }
+        while (true){
+            System.out.print("Enter first letter of ");
+            System.out.print("show, insert, delete, of find: ");
+            char choise = getChar();
+            switch (choise){
+                case 's':
+                    theHashTable.display();
+                    break;
+                case 'i':
+                    System.out.print("Enter key value to insert: ");
+                    aKey = getInt();
+                    aDataItem = new Item(aKey);
+                    theHashTable.insert(aDataItem);
+                    break;
+                case 'd':
+                    System.out.println("Enter key value to delete: ");
+                    aKey = getInt();
+                    theHashTable.delete(aKey);
+                    break;
+                case 'f':
+                    System.out.println("Enter key value to find: ");
+                    aKey = getInt();
+                    aDataItem = theHashTable.find(aKey);
+                    if (aDataItem != null){
+                        System.out.println("Found "+ aKey);
+                    }else
+                        System.out.println("Could not find "+aKey);
+                    break;
+                default:
+                    System.out.println("Invalid entry\n");
+            }
+        }
     }
     public static String getString() throws IOException{
         InputStreamReader isr = new InputStreamReader(System.in);
