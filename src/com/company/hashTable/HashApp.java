4684c007e91af3a94dfda5b1db8c71839a5df97f
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-16 23:30:24 +0300

    add method getString

--- src/com/company/hashTable/HashApp.java
+++ src/com/company/hashTable/HashApp.java
@@ -1,6 +1,8 @@
 package com.company.hashTable;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HashApp {
     public static void main(String[] args) throws IOException {
@@ -13,6 +15,13 @@ public class HashApp {
 
 
 
+
+    }
+    public String getString() throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
 
     }
     public int getInt() throws IOException{
