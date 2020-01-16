e00368121803f4899c6adc47d6f628168e39622f
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-16 23:26:15 +0300

    add HashApp

---
+++ src/com/company/hashTable/HashApp.java
@@ -0,0 +1,16 @@
+package com.company.hashTable;
+
+import java.io.IOException;
+
+public class HashApp {
+    public static void main(String[] args) throws IOException {
+        Item aDataItem;
+        int aKey, size, n, keysPerCell;
+        System.out.println("Enter size of hash table: ");
+        size = getInt();
+        System.out.println("Enter initial number of items: ");
+        n = getInt();
+        keysPerCell =10;
+
+    }
+}
